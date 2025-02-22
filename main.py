dbaf5b2de6397e1bd298219453aa5a4eb91af46c
Amit Dethe <26amitdethe@gmail.com>
2025-02-22 05:51:38 -0600

    updated main.py to handle CORS

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 import pandas as pd
 from model import recommend_movies_weighted
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 @app.get("/")
